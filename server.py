85e4be21061175c42d230f3d6cd0456077feca75
MikelDB <mikeldiezbuil@gmail.com>
2020-04-24 13:57:21 +0100

    Heroku parameters

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from app import app
+import os
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', ssl_context="adhoc")
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, host='0.0.0.0', port=port, ssl_context="adhoc")
